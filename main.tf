6ac8c8b5a665470431a29169d39b6af267baa51e
jmd603-shalini <shalini.r@jmangroup.com>
2026-06-09 11:08:07 +0000

    update:add storage account

--- main.tf
+++ main.tf
@@ -15,3 +15,11 @@ resource "azurerm_resource_group" "test" {
   name     = "rg-infracost-test"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "test" {
+  name                     = "infracosttest12345"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
