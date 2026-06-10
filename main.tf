288f5fd011eef865df22fd1d30507c6bee47577d
jmd603-shalini <shalini.r@jmangroup.com>
2026-06-10 05:27:17 +0000

    final commit

--- main.tf
+++ main.tf
@@ -22,4 +22,37 @@ resource "azurerm_storage_account" "test" {
   location                 = azurerm_resource_group.test.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  # Security improvement (already correct)
+  public_network_access_enabled = false
+
+  # Required FinOps tags (fixes tagging policy issue)
+  tags = {
+    Service     = "Storage"
+    Environment = "Dev"
+  }
+
+  blob_properties {
+    last_access_time_enabled = true
+  }
+}
+
+# Lifecycle policy (fixes governance recommendation)
+resource "azurerm_storage_management_policy" "test" {
+  storage_account_id = azurerm_storage_account.test.id
+
+  rule {
+    name    = "cleanup-rule"
+    enabled = true
+
+    filters {
+      blob_types = ["blockBlob"]
+    }
+
+    actions {
+      base_blob {
+        delete_after_days_since_modification_greater_than = 365
+      }
+    }
+  }
 }
